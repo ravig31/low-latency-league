436f95a8d95383ab14d043fde3fdae62f5184363
ravig31 <ravindugamage7@gmail.com>
2025-05-24 17:38:44 +1000

    Fix formatting and clean up makefile

--- Makefile
+++ Makefile
@@ -1,13 +1,10 @@
 CXX = g++
 CXXFLAGS =  -std=c++20 -Wall -Wextra -Ofast -ffast-math -flto -march=native -mtune=native -fomit-frame-pointer -fprefetch-loop-arrays -fno-plt -finline-functions -finline-limit=500
-PERFFLAGS = -B -e task-clock,context-switches,cpu-migrations,page-faults,cycles,instructions,branches,branch-misses,cache-references,cache-misses,L1-dcache-loads,L1-icache-loads,L1-icache-load-misses,LLC-loads,LLC-load-misses
+PERFFLAGS = -B -e task-clock,context-switches,cpu-migrations,page-faults,cycles,instructions,branches,branch-misses,cache-references,cache-misses,L1-dcache-loads,L1-dcache-load-misses,L1-icache-loads,L1-icache-load-misses,LLC-loads,LLC-load-misses
 MAKEFILE_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+
 all: test
 
-script: script.cpp
-	$(CXX) -std=c++20 -Wall -Wextra -g -o script script.cpp engine.cpp
-
-	
 test: tests.cpp
 	$(CXX) -std=c++20 -Wall -Wextra -g -g -o tests tests.cpp engine.cpp
 	./tests
@@ -17,31 +14,25 @@ submit: engine.cpp
 	$(CXX) $(CXXFLAGS) -shared -o engine.so engine.o
 	DATE_SUFFIX=$(shell date +%m%d_%H%M%S); \
 	BENCHMARK_FILE=benchmarks/results_$$DATE_SUFFIX.txt; \
-	perf stat $(PERFFLAGS) lll-bench $(MAKEFILE_DIR)engine.so -d 1 2>&1 | tee -a "$$BENCHMARK_FILE"
+	perf stat $(PERFFLAGS) ./lll-bench $(MAKEFILE_DIR)engine.so -d 1 2>&1 | tee -a "$$BENCHMARK_FILE"
 
 benchmark:
 	$(CXX) $(CXXFLAGS) -fPIC -c engine.cpp -o engine.o
 	$(CXX) $(CXXFLAGS) -shared -o engine.so engine.o
-	perf stat $(PERFFLAGS) lll-bench $(MAKEFILE_DIR)engine.so -d 1 
+	perf stat $(PERFFLAGS) ./lll-bench $(MAKEFILE_DIR)engine.so -d 1 
 
 record:
 	$(CXX) $(CXXFLAGS) -fPIC -c engine.cpp -o engine.o
 	$(CXX) $(CXXFLAGS) -shared -o engine.so engine.o
 	DATE_SUFFIX=$(shell date +%m%d_%H%M%S); \
 	BENCHMARK_FILE=records/records_$$DATE_SUFFIX.data; \
-	perf record -F 100 $(PERFFLAGS) -o $$BENCHMARK_FILE lll-bench $(MAKEFILE_DIR)engine.so -d 1
+	perf record -F 100 $(PERFFLAGS) -o $$BENCHMARK_FILE ./lll-bench $(MAKEFILE_DIR)engine.so -d 1
 
 flame:
 	$(CXX) $(CXXFLAGS) -fPIC -c engine.cpp -o engine.o
 	$(CXX) $(CXXFLAGS) -shared -o engine.so engine.o
-	perf record -F 99 -g -a lll-bench $(MAKEFILE_DIR)engine.so -d 1
+	perf record -F 99 -g -a ./lll-bench $(MAKEFILE_DIR)engine.so -d 1
 	perf script | ${HOME}/main/FlameGraph/stackcollapse-perf.pl | ${HOME}/main/FlameGraph/flamegraph.pl > flamegraph.svg
-
-callgrind:
-	$(CXX) $(CXXFLAGS) -fPIC -c engine.cpp -o engine.o
-	$(CXX) $(CXXFLAGS) -shared -o engine.so engine.o
-	valgrind --tool=callgrind lll-bench $(MAKEFILE_DIR)engine.so -d 1
-	cg_annotate callgrind.out.*
 
 clean:
 	rm -f tests engine.o engine.so script
--- engine.hpp
+++ engine.hpp
@@ -7,11 +7,7 @@
 #include <bitset>
 #include <cstdint>
 
-enum class Side : uint8_t
-{
-	BUY,
-	SELL
-};
+enum class Side : uint8_t { BUY, SELL };
 
 using IdType = uint32_t;
 using PriceType = uint16_t;
@@ -25,55 +21,53 @@ static constexpr uint16_t MAX_NUM_PRICES = 1024;
 static constexpr uint16_t BASE_PRICE = 3456;
 
 // You CANNOT change this
-struct Order
-{
-	IdType id; // Unique
-	PriceType price;
-	QuantityType quantity;
-	Side side;
+struct Order {
+  IdType id; // Unique
+  PriceType price;
+  QuantityType quantity;
+  Side side;
 };
 
 using PriceLevels = SortedFixedArray<LevelPriceType, MAX_NUM_PRICES>;
-using OrderLevels = std::array<CircularBuffer<IdType, MAX_ORDERS_PER_LEVEL>, MAX_NUM_PRICES>;
+using OrderLevels =
+    std::array<CircularBuffer<IdType, MAX_ORDERS_PER_LEVEL>, MAX_NUM_PRICES>;
 using Volumes = std::array<VolumeType[2], MAX_NUM_PRICES>;
 
 using OrderStore = std::array<Order, MAX_ORDERS>;
 using OrderBitSet = std::bitset<MAX_ORDERS>;
 
 // You CAN and SHOULD change this
-struct alignas(64) Orderbook
-{
-	alignas(64) SortedFixedArray<LevelPriceType, MAX_NUM_PRICES> buyLevels{};
-	alignas(64) std::array<CircularBuffer<IdType, MAX_ORDERS_PER_LEVEL>, MAX_NUM_PRICES> buyIds{};
+struct alignas(64) Orderbook {
+  alignas(64) SortedFixedArray<LevelPriceType, MAX_NUM_PRICES> buyLevels{};
+  alignas(64) std::array<CircularBuffer<IdType, MAX_ORDERS_PER_LEVEL>,
+                         MAX_NUM_PRICES> buyIds{};
 
-	alignas(64) SortedFixedArray<LevelPriceType, MAX_NUM_PRICES> sellLevels{};
-	alignas(64) std::array<CircularBuffer<IdType, MAX_ORDERS_PER_LEVEL>, MAX_NUM_PRICES> sellIds{};
+  alignas(64) SortedFixedArray<LevelPriceType, MAX_NUM_PRICES> sellLevels{};
+  alignas(64) std::array<CircularBuffer<IdType, MAX_ORDERS_PER_LEVEL>,
+                         MAX_NUM_PRICES> sellIds{};
 
-	alignas(64) std::array<VolumeType[2], MAX_NUM_PRICES> volumes{};
+  alignas(64) std::array<VolumeType[2], MAX_NUM_PRICES> volumes{};
 
-	alignas(64) std::array<Order, MAX_ORDERS> orders{};
-	alignas(64) std::bitset<MAX_ORDERS> ordersActive{};
+  alignas(64) std::array<Order, MAX_ORDERS> orders{};
+  alignas(64) std::bitset<MAX_ORDERS> ordersActive{};
 };
 
-extern "C"
-{
-	// Takes in an incoming order, matches it, and returns the number of matches
-	// Partial fills are valid
+extern "C" {
+// Takes in an incoming order, matches it, and returns the number of matches
+// Partial fills are valid
 
-	uint32_t match_order(Orderbook& orderbook, const Order& incoming) noexcept;
+uint32_t match_order(Orderbook &orderbook, const Order &incoming) noexcept;
 
-	// Sets the new quantity of an order. If new_quantity==0, removes the order
-	void modify_order_by_id(
-		Orderbook& orderbook,
-		IdType order_id,
-		QuantityType new_quantity
-	) noexcept;
+// Sets the new quantity of an order. If new_quantity==0, removes the order
+void modify_order_by_id(Orderbook &orderbook, IdType order_id,
+                        QuantityType new_quantity) noexcept;
 
-	// Returns total resting volume at a given price point
-	uint32_t get_volume_at_level(Orderbook& orderbook, Side side, PriceType price) noexcept;
+// Returns total resting volume at a given price point
+uint32_t get_volume_at_level(Orderbook &orderbook, Side side,
+                             PriceType price) noexcept;
 
-	// Performance of these do not matter. They are only used to check correctness
-	Order lookup_order_by_id(Orderbook& orderbook, IdType order_id);
-	bool order_exists(Orderbook& orderbook, IdType order_id);
-	Orderbook* create_orderbook();
+// Performance of these do not matter. They are only used to check correctness
+Order lookup_order_by_id(Orderbook &orderbook, IdType order_id);
+bool order_exists(Orderbook &orderbook, IdType order_id);
+Orderbook *create_orderbook();
 }
